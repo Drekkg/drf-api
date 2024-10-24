7adad0260dc60a729799bf8a8f4a724b799341d8
Derek Garnett <rizzagum@gmail.com>
2024-10-24 18:55:00 +0000

    FIX allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,8 +61,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
+
 # su pw derekg1971
 ALLOWED_HOSTS = ['8000-drekkg-drfapi-48anwstu1os.ws.codeinstitute-ide.net',
                  'vernon-makes-pancakes-c73f647bd403.herokuapp.com']
