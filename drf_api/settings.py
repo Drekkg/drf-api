72cd9b2918e8b47610ff9a416d28c4c9e5a9a158
Derek Garnett <rizzagum@gmail.com>
2024-10-26 11:09:43 +0000

    check  for 400error

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,8 +61,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
-
+# DEBUG = 'DEV' in os.environ
+DEBUG = True
 ALLOWED_HOSTS = ['https://vernon-makes-pancakes-c73f647bd403.herokuapp.com',
                  '8000-drekkg-drfapi-48anwstu1os.ws.codeinstitute-ide.net',
                  ]
