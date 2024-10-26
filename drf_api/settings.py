1c083405075f9b05bd4ea218e86a14d57d9a2ab0
Derek Garnett <rizzagum@gmail.com>
2024-10-26 18:50:52 +0000

    ADDS heroku allowed app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,9 +61,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
-ALLOWED_HOSTS = ['vernon-makes-pancakes-c73f647bd403.herokuapp.com',
+DEBUG = 'DEV' in os.environ
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),
                  '8000-drekkg-drfapi-48anwstu1os.ws.codeinstitute-ide.net',
                  ]
 
