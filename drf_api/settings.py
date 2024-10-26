b8636504194c93d582509aa9448102ad54ad6a71
Derek Garnett <rizzagum@gmail.com>
2024-10-26 19:11:51 +0000

    ADDS heroku allowed app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # DEBUG = 'DEV' in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),
+ALLOWED_HOSTS = ['.herokuapp.com',
                  '8000-drekkg-drfapi-48anwstu1os.ws.codeinstitute-ide.net',
                  ]
 
